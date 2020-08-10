6f7dbba9afad1a9c8c233c20e9824ef9babe2fac
Jvc <jhordyvc11>
2020-08-09 20:54:32 -0500

    modificación para mntProducto

--- Back/app.js
+++ Back/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//cors permisos
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -15,23 +16,25 @@ app.use((req, res, next) => {
 	next();
 });
 
-
-const staticRoute = path.join(__dirname, '../archivo/foto')
+//Verificación de fotos en el server
+const staticRoute = path.join(__dirname, '../public/imagenes')
 app.use('/sources', express.static(staticRoute))
 
-var docente=require('../Controller/MenuController');
-var catProducto=require('../Ruta/catProductoController');
+//invocación de metodo de rutas
+var catProducto=require('../Ruta/routecatProducto');
 var marca=require('../Ruta/routeMarcaProducto');
 var subirfoto=require('../Controller/subirFotoController');
-var producto=require('../Controller/productoController');
-var catUsuario=require('../Controller/catUsuarioController');
-app.use(marca);
-app.use(docente);
+var producto=require('../Ruta/routeProducto');
+var catUsuario=require('../Ruta/routeCatUsuario');
+//var docente=require('../Controller/MenuController');
 app.use(catProducto);
+app.use(marca);
 app.use(subirfoto);	
 app.use(producto);
 app.use(catUsuario);
+//app.use(docente);
 
+//creación del server
 app.listen(8888,function(error,res){
 	if(error) throw error;
 	else console.log("servidor corriendo perfectamente");
